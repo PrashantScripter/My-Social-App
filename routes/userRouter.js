e17a27c0dbf45c60a0755c9baf1f73fa5fad963d
Prashant Thakur <prashantthakurji90@gmail.com>
2024-09-21 14:35:49 +0530

    last commit

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,26 +10,34 @@ const router = express.Router();
 router.get('/profile', authenticate, async (req, res) => {
     const userId = req.user.userId;
     const userDetail = await User.findById(userId);
-    const posts = await Post.find({createdBy:userDetail});
-    return res.render('UserProfile',{posts,user:userId, userDetail});
+    const posts = await Post.find({ createdBy: userDetail });
+    return res.render('UserProfile', { posts, user: userId, userDetail });
 });
 
-router.get('/profile/:id', authenticate, async(req, res) => {
+router.get('/profile/:id', authenticate, async (req, res) => {
     const userId = req.user.userId;
+    const loggedInUser = await User.findById(userId);
+    console.log(loggedInUser._id);
     const specificUserId = req.params.id;
     const userDetail = await User.findById(specificUserId);
-    const posts = await Post.find({createdBy:userDetail});
-    return res.render('GetUserProfile',{posts,user:userId, userDetail});
+    const posts = await Post.find({ createdBy: userDetail });
+
+    const isFollowing = loggedInUser.following.some(followedUser => followedUser.toString() === userDetail._id.toString()); 
+    console.log(isFollowing);
+    const isYourFollower = loggedInUser.followers.some(yourFollower => yourFollower.toString() === userDetail._id.toString());
+    console.log(isYourFollower);
+
+    return res.render('GetUserProfile', { posts, userDetail, loggedInUser, isFollowing, isYourFollower });
 })
 
-router.get('/suggestion', authenticate, async(req, res) => {
+router.get('/suggestion', authenticate, async (req, res) => {
     const loggedInUserId = req.user.userId;
     const loggedInUser = await User.findById(loggedInUserId);
     const allUsers = await User.find({});
-    return res.render('UserSuggestion',{user:loggedInUserId, allUsers, loggedInUser});
+    return res.render('UserSuggestion', { user: loggedInUserId, allUsers, loggedInUser });
 });
 
-router.route('/follow/:id').post(authenticate,followUser);
+router.route('/follow/:id').post(authenticate, followUser);
 
 router.route('/signup').post(register).get((req, res) => {
     return res.render('SignupPage')
@@ -38,18 +46,18 @@ router.route('/signin').post(login).get((req, res) => {
     return res.render('LoginPage');
 });
 
-router.get('/logout',(req, res) => {
+router.get('/logout', (req, res) => {
     res.clearCookie('token').redirect('/');
 });
 
-router.route('/updateuser').put(updateUser).get(authenticate, async(req, res) => {
+router.route('/updateuser').put(updateUser).get(authenticate, async (req, res) => {
     const userId = req.user.userId;
     const user = await User.findById(userId);
-    res.render('UpdateUser', {user});
+    res.render('UpdateUser', { user });
 });
 
 
-router.delete('/account/delete',deleteUser);
+router.delete('/account/delete', deleteUser);
 
 
 module.exports = router;
